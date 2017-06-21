64f0ba6f105747af186db94434d697ae1e9a44de
Koen van der Kruk <koenvdkruk@gmail.com>
2017-06-21 10:42:32 +0200

    Added database functionalitey

--- Week5/5.2Server/src/calcws/Note.java
+++ Week5/5.2Server/src/calcws/Note.java
@@ -1,12 +1,40 @@
 package calcws;
 
+import java.util.Date;
+
 public class Note{
 	
 	private String text;
 	private int id;
+	private Date timeStamp;
 	
 	public Note(){
 		text = "";
 		id = 0;
+		timeStamp = new Date();
+	}
+	
+	public void setText(String tex){
+		text = tex;
+	}
+	
+	public void setId(int i){
+		id = i;
+	}
+	
+	public void setDate(Date stamp){
+		timeStamp = stamp;
+	}
+	
+	public String getText(){
+		return text;
+	}
+	
+	public int getId(){
+		return id;
+	}
+	
+	public Date getDate(){
+		return timeStamp;
 	}
 }
--- Week5/5.2Server/src/calcws/Notes.java
+++ Week5/5.2Server/src/calcws/Notes.java
@@ -5,28 +5,86 @@ import javax.jws.WebService;
 import javax.jws.WebParam;
 import java.util.List;
 import java.util.ArrayList;
+import java.sql.*;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 @WebService
 public class Notes {
-    
+	// JDBC driver name and database URL
+    private String JDBC_DRIVER = "org.sqlite.JDBC";  
+	private String DB_URL = "jdbc:sqlite:";
+		
+    private Connection conn = null;
+    private Statement stmt = null;
+	
+	
+	private List<Note> allNotes;
+	
+	public Notes(){
+		allNotes = new ArrayList<Note>();
+		createDatabase();
+		//readDatabase();
+	}
 
+	private void executeSqlQueryWithoutResult(String query){
+			try{
+				Class.forName("org.sqlite.JDBC");
+				conn = DriverManager.getConnection(DB_URL);
+				stmt = conn.createStatement();
+				stmt.executeUpdate(query);
+				stmt.close();
+				conn.close();
+			}
+			catch(SQLException se){
+				//Handle errors for JDBC
+				se.printStackTrace();
+			}catch(Exception e){
+				//Handle errors for Class.forName
+				e.printStackTrace();
+			}finally{
+				//finally block used to close resources
+				try{
+					if(stmt!=null)
+						stmt.close();
+					}catch(SQLException se2){
+					}// nothing we can do
+					try{
+						if(conn!=null)
+						conn.close();
+					}catch(SQLException se){
+						se.printStackTrace();
+				}//end finally try
+			}
+	}
+
+	private void createDatabase(){
+		System.out.println("Database Created");
+		String sql;
+		sql = "CREATE TABLE IF NOT EXISTS Notes(id INTEGER,noteText TEXT,noteTimeStamp DATE, PRIMARY KEY (id));";
+		executeSqlQueryWithoutResult(sql);
+	}
+	
+	
 	public boolean addNote(@WebParam(name="text") String tekst){
+		String sql;
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+		String date = sdf.format(new Date());
+		sql = "INSERT INTO Notes (noteText, noteTimeStamp) VALUES ('"+tekst+"', '"+date+");";
+		executeSqlQueryWithoutResult(sql);
 		System.out.println("request addNote(" + tekst + ")");
 		return false;
 	}
 	
 	public boolean removeNote(@WebParam(name="id") int  noteId){
+		String sql = "DELETE FROM Notes WHERE id='"+noteId+"';";
+		executeSqlQueryWithoutResult(sql);
 		System.out.println("request removeNote(" + noteId + ")");
 		return false;
 	}
 
 	public List<Note> getNotes(){
 
-	
-		
-		List<Note> allNotes = new ArrayList<Note>();
-		Note theNote = new Note();
-		allNotes.add(theNote);
 		return allNotes;
 	}
 	
